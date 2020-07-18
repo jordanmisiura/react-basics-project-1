e1498e5e4ac2dd78a3d61290b81a9f4453c288c2
jordanmisiura <jordanmisiura@gmail.com>
2020-07-17 21:44:17 -0700

    Updates

---
+++ precache-manifest.6ba20c77309d174b2d1bb3a1d5424a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985924ca5d801af45d4598a1fc29ab60",
+    "url": "/react-basics-project-1/index.html"
+  },
+  {
+    "revision": "f48a00a091616fea902f",
+    "url": "/react-basics-project-1/static/css/main.031a4d67.chunk.css"
+  },
+  {
+    "revision": "ef594503646c778d0fba",
+    "url": "/react-basics-project-1/static/js/2.5cd5d515.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics-project-1/static/js/2.5cd5d515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f48a00a091616fea902f",
+    "url": "/react-basics-project-1/static/js/main.e6789fdc.chunk.js"
+  },
+  {
+    "revision": "21ac11c3702c272d6bb9",
+    "url": "/react-basics-project-1/static/js/runtime-main.743e5568.js"
+  }
+]);
